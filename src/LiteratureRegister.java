8b6af1d9e1d8da702dc5e290c706d26adc9b904f
Erlend Holseker <erlend.holseker@gmail.com>
2019-03-04 12:45:00 +0100

    Changed the LiteratureRegister class. Now using ArrayList instead of HashMap.

--- src/LiteratureRegister.java
+++ src/LiteratureRegister.java
@@ -1,61 +1,55 @@
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 
 public class LiteratureRegister
 {
 
-    private HashMap<String, Newspaper> newspaperMap;
+    private ArrayList<Newspaper> newspaperCollection;
 
     /**
-     * Makes a new registry to store different newspapers
+     * Creates a new registry to store different newspapers
      */
     public LiteratureRegister()
     {
-        newspaperMap = new HashMap<>();
+        newspaperCollection = new ArrayList<>();
     }
 
     /**
      * Adds new newspapers in the register.
-     * @param title title of the newspaper
-     * @param genre genre of the newspaper
-     * @param publisher genre of the newspaper
-     * @param numberOfPublishments  number of publisments of the newspaper
-     */
-    public void addNewspaper(String title, String genre,
-                             String publisher, int numberOfPublishments)
+     * @param newspaper The newspaper to be added to the register.
+     * */
+    public void addNewspaper(Newspaper newspaper)
     {
-        Newspaper newspaper = new Newspaper(title, publisher, genre, numberOfPublishments);
-        this.newspaperMap.put(title, newspaper);
+        this.newspaperCollection.add(newspaper);
     }
 
-    /**
-     * Prints information about the newspaper, based on the title
-     * @param title the title to retrieve information about
-     */
-    public void printNewspaperInfo(String title)
+    public Iterator<Newspaper> getIterator()
     {
-        if(newspaperMap.containsKey(title))
+        return this.newspaperCollection.iterator();
+    }
+
+    public ArrayList<Newspaper> getAllNewspapers()
+    {
+        return this.newspaperCollection;
+    }
+
+    public void listAllNewspapers()
+    {
+        for (Newspaper newspaper : this.newspaperCollection)
         {
-            Newspaper newspaper = this.newspaperMap.get(title);
-            System.out.println("Title: " + title);
-            System.out.println("Publisher: " + newspaper.getPublisher());
-            System.out.println("Genre: " + newspaper.getGenre());
-            System.out.println("Number of publishments: " + newspaper.getNumberOfPublishments());
-        }
-        else
-        {
-            System.out.println("No matches found.");
+            System.out.println("Title: " + newspaper.getTitle()
+                                + " Number of issues: " + newspaper.getNumberOfPublishments());
         }
     }
 
     /**
      * Removes a newspaper object from the registry
      * @param title the title of the newspaper to be removed
-     */
     public void removeNewspaper(String title)
     {
         this.newspaperMap.remove(title);
         System.out.println("Removed " + title + " from the registry.");
     }
-
+    */
 }
