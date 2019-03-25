99bc6aa55741e48f2d71cb60355e3fa9443a028e
Arvin Khodabandeh <arvin.khodabandeh@gmail.com>
2019-03-25 11:34:50 +0100

    Added comments to "Periodical" and "Magazine".

--- src/Magazine.java
+++ src/Magazine.java
@@ -1,24 +1,18 @@
 /**
+ *  The Magazine class makes objects of the type "Magazine". This class
+ *  inherits from the "Periodical" class. It does not contain any fields on its own.
  *
- *
- * @author
- * @version
+ * @author  Arvin Khodabandeh, Erlend Holseker & Isak Gamnes Sneltvedt
+ * @version 0.1 (2019.03.25)
  */
 
 public class Magazine extends Periodical
 {
     public Magazine(String title, String publisher, String publishYear, String language, String genre,
-                    int price, int quantity, int numberOfYearlyPublications)
+                    int price, int quantityInStock, int numberOfYearlyPublications)
     {
-        super(numberOfYearlyPublications);
-        setTitle(title);
-        setPublisher(publisher);
-        setPublishYear(publishYear);
-        setLanguage(language);
-        setGenre(genre);
-        setPrice(price);
-        setQuantity(quantity);
-        setNumberOfYearlyPublications(numberOfYearlyPublications);
+        super(title, publisher, publishYear, language, genre,
+        price, quantityInStock, numberOfYearlyPublications);
 
     }
 }
