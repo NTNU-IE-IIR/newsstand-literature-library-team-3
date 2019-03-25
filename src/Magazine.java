2966dc45a1cff81033f6ddc075cc20a1c520947c
Arvin Khodabandeh <arvin.khodabandeh@gmail.com>
2019-03-25 13:26:18 +0100

    Fixed the parameters of Magazine constructor.

--- src/Magazine.java
+++ src/Magazine.java
@@ -8,17 +8,9 @@
 public class Magazine extends Periodical
 {
     public Magazine(String title, String publisher, String publishYear, String language, String genre,
-                    int price, int quantity, int numberOfYearlyPublications)
+                    int price, int quantityInStock, int numberOfYearlyPublications)
     {
-        super(numberOfYearlyPublications);
-        setTitle(title);
-        setPublisher(publisher);
-        setPublishYear(publishYear);
-        setLanguage(language);
-        setGenre(genre);
-        setPrice(price);
-        setQuantity(quantity);
-        setNumberOfYearlyPublications(numberOfYearlyPublications);
+        super(title, publisher, publishYear, language, genre, price, quantityInStock);
 
     }
 }
