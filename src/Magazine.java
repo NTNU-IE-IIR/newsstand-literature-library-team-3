6591bd38bc0ec9f371e2f45a4ba9fe83d3f567b7
Arvin Khodabandeh <arvin.khodabandeh@gmail.com>
2019-03-27 11:35:05 +0100

    Fixed the parameters of Magazine constructor and added comments to the class.

--- src/Magazine.java
+++ src/Magazine.java
@@ -1,8 +1,8 @@
 /**
+ *  Represents a magazine. This class inherits from the periodical class.
  *
- *
- * @author
- * @version
+ * @author  Arvin Khodabandeh, Erlend Holseker & Isak Gamnes Sneltvedt
+ * @version 0.1 (2019.3.27)
  */
 
 public class Magazine extends Periodical
@@ -10,7 +10,7 @@ public class Magazine extends Periodical
     public Magazine(String title, String publisher, String publishYear, String language, String genre,
                     int price, int quantityInStock, int numberOfYearlyPublications)
     {
-        super(title, publisher, publishYear, language, genre, price, quantityInStock);
+        super(title, publisher, publishYear, language, genre, price, quantityInStock, numberOfYearlyPublications);
 
     }
 }
