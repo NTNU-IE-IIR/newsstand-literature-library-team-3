33a2c1956a11d9024c2bfd03ab325d9460cb5dd8
Arvin Khodabandeh <arvin.khodabandeh@gmail.com>
2019-05-01 19:26:56 +0200

    Moved the reduceQuantityByOne from ApplicationUI to Cart.

--- src/Logic/Cart.java
+++ src/Logic/Cart.java
@@ -35,6 +35,7 @@ public class Cart
         else
         {
             updateTotalPricePlus(literatureToAdd);
+            literatureToAdd.reduceQuantityByOne();
             cart.add(literatureToAdd);
         }
     }
